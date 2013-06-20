9b6bbddbdabf87a796bfbb2eaef84ae0959b618e
Nate Clark <nate@developerauction.com>
2013-06-19 17:24:20 -0700

    adds NewRelic instrumentation to update_metrics calls

--- lib/has_metrics.rb
+++ lib/has_metrics.rb
@@ -6,5 +6,17 @@ module HasMetrics
   def self.included(base)
     base.send :include, Metrics
     base.send :include, Segmentation
+
+    if defined?(::NewRelic)
+      base.send :include, ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
+      base.class_eval do
+        ::NewRelic::Agent.logger.debug "Installing instrumentation for #{base.to_s}Metrics"
+        add_transaction_tracer :update_metrics!, category: :task
+        class << self
+          include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
+          add_transaction_tracer :update_all_metrics!, category: :task
+        end
+      end
+    end
   end
 end
--- lib/has_metrics/version.rb
+++ lib/has_metrics/version.rb
@@ -1,3 +1,3 @@
 module HasMetrics
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
 end
